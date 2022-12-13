9be8eeaa5c6c7ab70738c4ce18d6716ad7fa5477
Ciolpan Sergiu <ciolpan.sergiu64@gmail.com>
2022-12-13 20:07:59 +0200

    create NavbarContent component

--- guitar-shop/src/components/Navbar.tsx
+++ guitar-shop/src/components/Navbar.tsx
@@ -1,53 +1,56 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../styles/NavbarStyle.scss";
 import { BsFillTelephoneFill, BsSearch } from "react-icons/bs";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { FaRegUserCircle } from "react-icons/fa";
+import NavbarMenu from "./NavbarMenu";
+import NavbarContent from "./NavbarContent";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const openMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-first-row">
-        <h3 className="website-logo">SergiuGuitars</h3>
-        <Link to="/contact">
-          <span className="telephone-icon nav-icon">
-            <BsFillTelephoneFill />
-          </span>
-          +0123456789
-        </Link>
-        <Link to="/account">
-          <span className="user-icon nav-icon">
-            <FaRegUserCircle />
-          </span>
-        </Link>
-        <Link to="/shopping-cart">
-          <span className="shopping-cart-icon nav-icon">
-            <HiOutlineShoppingCart />
-          </span>
-        </Link>
-        <div className="bars-menu">
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+    <div className="navbar-container">
+      <nav className="navbar">
+        <div className="navbar-first-row">
+          <h3 className="website-logo">SergiuGuitars</h3>
+          <Link to="/contact">
+            <span className="telephone-icon nav-icon">
+              <BsFillTelephoneFill />
+            </span>
+            +0123456789
+          </Link>
+          <Link to="/account">
+            <span className="user-icon nav-icon">
+              <FaRegUserCircle />
+            </span>
+          </Link>
+          <Link to="/shopping-cart">
+            <span className="shopping-cart-icon nav-icon">
+              <HiOutlineShoppingCart />
+            </span>
+          </Link>
+          <NavbarMenu handleClick={openMenu} />
         </div>
-      </div>
 
-      <form className="search-box">
-        <span className="search-box__search-icon nav-icon">
-          <BsSearch />
-        </span>
-        <input
-          className="search-box__input"
-          type="text"
-          placeholder="Searching something specific?"
-        />
-        <button className="search-box__button">Search</button>
-      </form>
-
-      <ul className="category-navigation-ul">
-        {/* <li className="category"></li> */}
-      </ul>
-    </nav>
+        <form className="search-box">
+          <span className="search-box__search-icon nav-icon">
+            <BsSearch />
+          </span>
+          <input
+            className="search-box__input"
+            type="text"
+            placeholder="Searching something specific?"
+          />
+          <button className="search-box__button">Search</button>
+        </form>
+      </nav>
+      {isMenuOpen && <NavbarContent />}
+    </div>
   );
 }
--- guitar-shop/src/components/NavbarMenu.tsx
+++ guitar-shop/src/components/NavbarMenu.tsx
@@ -1,1 +1,12 @@
-export default function NavbarMenu() {}
+import "../styles/NavbarMenuStyle.scss";
+
+export default function NavbarMenu({ handleClick }: any) {
+  return (
+    <div className="bars-menu" onClick={handleClick}>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+  );
+}
