252cf3e1437880652d41aa301fc078339ad9b841
Ciolpan Sergiu <ciolpan.sergiu64@gmail.com>
2023-01-18 19:30:51 +0200

    finish keyboard and synth categories

--- guitar-shop/src/data/LevelTwoCategories/keyboardsAndSynth.ts
+++ guitar-shop/src/data/LevelTwoCategories/keyboardsAndSynth.ts
@@ -1,4 +1,5 @@
-import electricGuitarSubcategories from "../LevelThreeCategories/GuitarsAndBasses/electricGuitarsData.json";
+import keyboardsSubcategories from "../LevelThreeCategories/KeyboardsAndSynth/keyboardsData.json";
+import synthesizerSubcategories from "../LevelThreeCategories/KeyboardsAndSynth/synthesizersData.json";
 
 const keyboardAndSynthData = [
   {
@@ -6,56 +7,50 @@ const keyboardAndSynthData = [
     alt: "A simple keyboard",
     title: "Keyboards",
     pageLink: "/keyboards",
-    subcategories: electricGuitarSubcategories,
-  },
-  {
-    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/stage_piano.jpg",
-    alt: "Smaller than a huge piano, a stage piano it's a piano for stages",
-    title: "Stage Pianos",
-    pageLink: "/stage-pianos",
-    subcategories: electricGuitarSubcategories,
-  },
-  {
-    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/classic_piano.jpg",
-    alt: "A good, old, classic piano everyone know",
-    title: "Classic Pianos",
-    pageLink: "/classic-pianos",
-    subcategories: electricGuitarSubcategories,
-  },
-  {
-    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/digital_piano.jpg",
-    alt: "A modern version of piano called digital piano",
-    title: "Ditital Pianos",
-    pageLink: "/digital-pianos",
-    subcategories: electricGuitarSubcategories,
+    subcategories: keyboardsSubcategories,
   },
   {
     src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/synthesizer.jpg",
     alt: "A synthesizer",
     title: "Synthesizers",
     pageLink: "/synthesizers",
-    subcategories: electricGuitarSubcategories,
+    subcategories: synthesizerSubcategories,
+  },
+  {
+    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/piano.jpg",
+    alt: "A piano",
+    title: "Pianos",
+    pageLink: "/pianos",
+  },
+  {
+    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/grand_piano.jpg",
+    alt: "A grand piano",
+    title: "Grand Pianos",
+    pageLink: "/grand-pianos",
+  },
+  {
+    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/digital_piano.jpg",
+    alt: "A digital piano",
+    title: "Digital Pianos",
+    pageLink: "/digital-pianos",
+  },
+  {
+    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/stage_piano.jpg",
+    alt: "A stage piano",
+    title: "Stage Pianos",
+    pageLink: "/stage-pianos",
   },
   {
     src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/electric_organ.jpg",
     alt: "An electric organ. A pipeless organ",
     title: "Electric Organs",
     pageLink: "/electric-organs",
-    subcategories: electricGuitarSubcategories,
   },
   {
     src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/classic_organ.jpg",
     alt: "An old, classic, piped organ",
     title: "Classic Organs",
     pageLink: "/classic-organs",
-    subcategories: electricGuitarSubcategories,
-  },
-  {
-    src: "/images/category/LevelTwoCategories/KeyboardsAndSynth/keyboard_cover_bag.jpg",
-    alt: "A keyboard cover bag",
-    title: "Keyboard Cover Bags",
-    pageLink: "/keyboard-cover-bags",
-    subcategories: electricGuitarSubcategories,
   },
 ];
 
